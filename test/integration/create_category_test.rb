095a9dfa58c2cf20770f3f40c75ffb8ed8f3df07
Pablo Gandarillas <XV.I.MCMLXXXIX@gmail.com>
2024-04-14 01:55:54 +0200

    Create integration test for invalid category creation attempt

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
   test "getting the new category form and create category" do
@@ -11,5 +11,16 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_response :success
     assert_match "Sports", response.body
+  end
+
+  test "get new category form and reject invalid category submission" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: " " } }
+    end
+    assert_match "errors", response.body
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
   end
 end
